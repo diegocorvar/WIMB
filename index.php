512e0db786ecc651034a07f84b088a8e43b5c226
mora8292 <alexmartincarrillo19@hotmail.com>
2026-02-26 14:03:21 -0600

    pasajeros boton

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         <a href="./inicio_sesion.php"><li>Inicio de sesión</li></a>
     </ul>
     <ul>
-        <a href="./pasajeros.php"><li>Inicio de sesión</li></a>
+        <a href="./pasajeros.php"><li>Pasajeros</li></a>
     </ul>
 </body>
 </html>
