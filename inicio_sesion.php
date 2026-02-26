5dce850b1f7df0794bc4d80d07365b52c7addd15
diegocorvar <diegoalonso.DACV@gmail.com>
2026-02-26 13:51:37 -0600

    creación de index.php para ir a las diferentes paginas + cambios en inicio_sesion.php

---
+++ inicio_sesion.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="./styles/diego-styles.css"/>
+    <link rel="stylesheet" href="./styles/normilize.css"/>
+</head>
+<body style="background-color: #f3e1b6">
+    <main class="contenedor-inicio-sesion">
+        <header>
+            <a><img class="logo-inicio-sesion" src="./assets/images/logos/logo-verde-oscuro.svg"/></a>
+            <h1 class="titulo-inicio-sesion">Inicia Sesión</h1>
+        </header>
+        <div class="contenedor-datos-inicio-sesion">
+            <form>
+                <label for="username" class="inicio-sesion">Usuario:</label>
+                <input type="text" id="username" name="username" placeholder="Nombre de usuario"/>
+
+                <label for="user-password" class="inicio-sesion">Contraseña:</label>
+                <input type="password" id="user-password" name="user-password" placeholder="Contraseña"/>
+
+                <button type="submit" class="inicio-sesion-btn">Iniciar Sesión</button>
+            </form>
+        </div>
+        <a href="#" class="ir-a-registro"><p>Resgistrarme</p></a>
+    </main>
+</body>
+</html>
